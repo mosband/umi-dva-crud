08758ac8a79332cf14f7862820d9b09050508cd8
mosband <mosband@163.com>
2020-06-07 23:51:27 +0800

    请求接口展示数据

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Table, Tag, Space } from 'antd';
+import styles from './index.less';
+import { connect } from 'umi';
+import { UsersModelState } from './model';
+
+interface IAppProps {
+  users: UsersModelState;
+}
+
+const App: React.FunctionComponent<IAppProps> = props => {
+  const { users } = props;
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text: any) => <a>{text}</a>,
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'create_time',
+      key: 'create_time',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text: any, record: any) => (
+        <Space size="middle">
+          <a>Edit</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <div className={styles.container}>
+      <Table
+        className={`table-box ${styles.tableBox}`}
+        columns={columns}
+        dataSource={users.items}
+      />
+    </div>
+  );
+};
+
+export default connect(state => {
+  return state;
+})(App);
